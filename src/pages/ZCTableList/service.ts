87d38e10de6f7fa4ea2c2a67de1c08b88c736db7
zilan830 <zilan830@163.com>
2020-08-10 13:29:36 +0800

    nginx

--- src/pages/ZCTableList/service.ts
+++ src/pages/ZCTableList/service.ts
@@ -2,10 +2,9 @@ import { request } from 'umi';
 import { TableListParams } from './data.d';
 
 export async function getData(params?: TableListParams) {
-  const { REACT_APP_ENV } = process.env;
   return request<{
     data: [];
-  }>(REACT_APP_ENV?'http://101.37.145.31:7001/user':'/api/user', {
+  }>('/api/user', {
     params,
   });
 }
